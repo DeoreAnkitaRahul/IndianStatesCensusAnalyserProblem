bb576683bb0923a6e54d68ec7c8a11157e788ef7
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-03-24 13:47:54 +0530

    Printing Welcome Msg

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndianStatesCensusAnalyserProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Pay Roll MultiThreading");
+        }
+    }
+}
